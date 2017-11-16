ec7e1009039805c288fcd7c3fbbeb6339ca43862
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-11-16 21:53:10 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,9 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(email){
+  return email.map(function(emailData)){
+    return new IcebreakerResponse(emailData[0], emailData[1])
+    
+  })
+}
